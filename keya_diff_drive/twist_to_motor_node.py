1f13fb1a93e75553a7dbc13de6cc6217c8ce6f2b
Matt Matt <matthew.mattar@canterbury.ac.nz>
2023-07-12 14:36:57 +1200

    .

--- keya_diff_drive/twist_to_motor_node.py
+++ keya_diff_drive/twist_to_motor_node.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
-import asyncio
 from functools import cached_property
 from std_msgs.msg import Float32, String
+from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
 from rcl_interfaces.msg import SetParametersResult
 from keya_diff_drive.motor_driver import MotorDriver
+from transforms3d.euler import euler2quat
 
 import rclpy
 
 from rclpy.node import Node
-import numpy
+import numpy as np
 
 class TwistToMotors(Node):
   def parameters_callback(self, params):
@@ -53,13 +54,15 @@ class TwistToMotors(Node):
     self.declare_parameters('', parameters=params)
     self._wheel_separation = self.get_parameter('wheel_separation').value
     self._wheel_radius = self.get_parameter('wheel_radius').value
-    self._wheel_circum = 2 * numpy.pi * self._wheel_radius
+    self._wheel_circum = 2 * np.pi * self._wheel_radius
 
     self._twist_topic = self.get_parameter('twist_topic').value
     self._odom_topic = self.get_parameter('odom_topic').value
 
     self._publish_odom = self.get_parameter('publish_odom').value
     self._publish_motors = self.get_parameter('publish_motors').value
+
+    self._odom_covar_scale = 0.01
 
     self.add_on_set_parameters_callback(self.parameters_callback)
   
@@ -78,7 +81,34 @@ class TwistToMotors(Node):
     self.odom_timer = self.create_timer(odometry_period, self.publish_odometry)
 
     self.twist_sub = self.twist_subscriber()
+
+    if self._publish_odom:
+      self.old_pos_l = 0
+      self.old_pos_r = 0
   
+      # setup message
+      self.odom_msg = Odometry()
+      self.odom_msg.header.frame_id = self.odom_frame
+      self.odom_msg.child_frame_id = self.base_frame
+      self.odom_msg.pose.pose.position.x = 0.0
+      self.odom_msg.pose.pose.position.y = 0.0
+      self.odom_msg.pose.pose.position.z = 0.0    # always on the ground, we hope
+      self.odom_msg.pose.pose.orientation.x = 0.0 # always vertical
+      self.odom_msg.pose.pose.orientation.y = 0.0 # always vertical
+      self.odom_msg.pose.pose.orientation.z = 0.0
+      self.odom_msg.pose.pose.orientation.w = 1.0
+      self.odom_msg.twist.twist.linear.x = 0.0
+      self.odom_msg.twist.twist.linear.y = 0.0  # no sideways
+      self.odom_msg.twist.twist.linear.z = 0.0  # or upwards... only forward
+      self.odom_msg.twist.twist.angular.x = 0.0 # or roll
+      self.odom_msg.twist.twist.angular.y = 0.0 # or pitch... only yaw
+      self.odom_msg.twist.twist.angular.z = 0.0
+      
+      # store current location to be updated. 
+      self.x = 0.0
+      self.y = 0.0
+      self.theta = 0.0
+
 
   @cached_property
   def left_wheel_publisher(self):
@@ -101,9 +131,58 @@ class TwistToMotors(Node):
 
   def publish_odometry(self):
     if self._publish_odom:
+      self.odom_msg.header.stamp = self.get_clock().now()
+
       message = self.motor_driver.get_encoders()
-      self.get_logger().info(message)
+      left_diff, right_diff = message.split(':')
+      forward, ccw = self.diff2twist(left_diff, right_diff)
+    
+      self.odom_msg.twist.twist.linear.x = forward
+      self.odom_msg.twist.twist.angular.z = ccw
+  
+      self.new_pos_l = left_diff
+      self.new_pos_r = right_diff
+      # Position
+      delta_pos_l = self.new_pos_l - self.old_pos_l
+      delta_pos_r = self.new_pos_r - self.old_pos_r
+      
+      self.old_pos_l = self.new_pos_l
+      self.old_pos_r = self.new_pos_r
+            
+      # counts to metres
+      delta_pos_l_m = delta_pos_l * self._wheel_circum
+      delta_pos_r_m = delta_pos_r * self._wheel_circum
+  
+      # Distance travelled
+      d = (delta_pos_l_m+delta_pos_r_m) / 2.0  # delta_ps
+      th = (delta_pos_r_m-delta_pos_l_m) / self._wheel_separation # works for small angles
+  
+      xd = np.cos(th)*d
+      yd = -np.sin(th)*d
+  
+      # Pose: updated from previous pose + position delta
+      self.x += np.cos(self.theta)*xd - np.sin(self.theta)*yd
+      self.y += np.sin(self.theta)*xd + np.cos(self.theta)*yd
+      self.theta = (self.theta + th) % (2*np.pi)
+      
+      # fill odom message and publish
+      
+      self.odom_msg.pose.pose.position.x = self.x
+      self.odom_msg.pose.pose.position.y = self.y
+      q = euler2quat(0.0, 0.0, self.theta)
+      self.odom_msg.pose.pose.orientation.z = q[2] # math.sin(self.theta)/2
+      self.odom_msg.pose.pose.orientation.w = q[3] # math.cos(self.theta)/2
+  
+      self.odom_msg.twist.covariance[0]  = forward * self._odom_covar_scale
+      self.odom_msg.twist.covariance[7]  = forward * self._odom_covar_scale
+      self.odom_msg.twist.covariance[14] = forward * self._odom_covar_scale
+      
+      
+      # ... and publish!
       self.wheel_odometry_publisher.publish(String(data=message))
+      if self.publish_tf:
+          self.tf_publisher.sendTransform(self.tf_msg)            
+    
 
   def twist2diff(self, forward, ccw):
     angular_to_linear = ccw * (self._wheel_separation / 2.0) 
@@ -129,7 +208,6 @@ class TwistToMotors(Node):
   
   def destroy_node(self):
     self.odom_timer.destroy()
-    self.vel_timer.destroy()
     self.motor_driver.serial.close()
     super().destroy_node()
 
